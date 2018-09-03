dda975fc88ba82ea8a992bc5f2fec846ffd36a65
condrint <condrint@gmail.com>
2018-09-03 10:06:45 -0700

    added client side js

---
+++ client/react_client/src/App.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+const axios = require('axios');
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      nodesCreated: false,
+      simulating: false,
+      resistorInputValue: '',
+      resistorNode1InputValue: '',
+      resistorNode2InputValue: '',
+      powerSupplyInputValue: '',
+      powerSupplyNode1InputValue: '',
+      powerSupplyNode2InputValue: '',
+      wireNode1InputValue: '',
+      wireNode2InputValue: '',
+      deleteNode1InputValue: '',
+      deleteNode2InputValue: '',
+      nodeDimensions: [],
+      rowsOfNodes: '',
+      colOfNodes: '',
+      sessionID: '',
+      edges: []
+    }
+  }
+
+  handleNodeSubmit(e){
+
+  }
+  
+  handleResistorSubmit(e){
+
+  }
+  handlePowerSupplySubmit(e){
+    
+  }
+  handleWireSubmit(e){
+    
+  }
+  handleDeleteEdgesSubmit(e){
+    
+  }
+  handleResistorInputChange(e){
+
+  }
+  handleResistorNode1InputChange(e){
+
+  }
+  handleResistorNode2InputChange(e){
+
+  }
+  handlePowerSupplyInputChange(e){
+
+  }
+  handlePowerSupplyNode1InputChange(e){
+  
+  }
+  handlePowerSupplyNode2InputChange(e){
+  
+  }
+  handleWireNode1InputChange(e){
+  
+  }
+  handleWireNode2InputChange(e){
+
+  }
+  handleDeleteNode1InputChange(e){
+  
+  }
+  handleDeleteNode2InputChange(e){
+
+  }
+  handleRowChange(e){
+
+  }
+  handleColChange(e){
+
+  }
+  simulate(){
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="inputContainer">
+            <form onSubmit={this.handleNodeSubmit} id="nodeForm">
+              <select name="rowlist" form="nodeForm" value={this.state.rowsOfNodes} onChange={this.handleRowChange}>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+              </select>
+              <select name="collist" form="nodeForm" value={this.state.colOfNodes} onChange={this.handleColChange}>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+              </select>
+              <input className="button" id="AddInput" type="submit" value="create nodes"/>
+            </form>
+            <hr/>
+            <form onSubmit={this.handleResistorSubmit}>
+              <input className="textInput" type="text" name="firstNodeResistorValue" placeholder="first node" value={this.state.resistorNode1InputValue} onChange={this.handleResistorNode1InputChange}/>
+              <input className="textInput" type="text" name="secondNodeResistorValue" placeholder="second node" value={this.state.resistorNode2InputValue} onChange={this.handleResistorNode2InputChange}/>
+              <input className="textInput" type="text" name="resistorValue" placeholder="Ω" value={this.state.resistorInputValue} onChange={this.handleResistorInputChange}/>
+              <input className="button" type="submit" value="create resistor"/>
+            </form>
+            <form onSubmit={this.handlePowerSupplySubmit}>
+              <input className="textInput" type="text" name="firstNodePowerSupplyValue" placeholder="first node" value={this.state.powerSupplyNode1InputValue} onChange={this.handlePowerSupplyNode1InputChange}/>
+              <input className="textInput" type="text" name="secondNodePowerSupplyValue" placeholder="second node" value={this.state.powerSupplyNode2InputValue} onChange={this.handlePowerSupplyNode2InputChange}/>
+              <input className="textInput" type="text" name="powerSupplyValue" placeholder="V" value={this.state.powerSupplyInputValue} onChange={this.handlePowerSupplyInputChange}/>
+              <input className="button" type="submit" value="create powersupply"/>
+            </form>
+            <form onSubmit={this.handleWireSubmit}>
+              <input className="textInput" type="text" name="firstNodeWireValue" placeholder="first node" value={this.state.wireNode1InputValue} onChange={this.handleWireNode1InputChange}/>
+              <input className="textInput" type="text" name="secondNodeWireValue" placeholder="second node" value={this.state.wireNode2InputValue} onChange={this.handleWireNode2InputChange}/>
+              <input className="button" type="submit" value="create wire"/>
+            </form>
+            <hr/>
+            <form onSubmit={this.handleDeleteEdgeSubmit}>
+              <input className="textInput" type="text" name="firstNodeValue" placeholder="first node" value={this.state.deleteNode1InputValue} onChange={this.handleDeleteNode1InputChange}/>
+              <input className="textInput" type="text" name="secondNodeValue" placeholder="second node" value={this.state.deleteNode2InputValue} onChange={this.handleDeleteNode2InputChange}/>
+              <input className="button" type="submit" value="delete component"/>
+            </form>
+            <hr/>
+            <button type="button" onClick={this.simulate}>simulate</button>
+            <hr/>
+        </div>
+        <div>{this.state.edges}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
